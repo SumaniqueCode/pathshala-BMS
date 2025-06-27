84716341e88a0db2c51d801441240bccaca144d3
Suman Regmi <limitlessplaying@gmail.com>
2025-06-27 17:02:49 +0545

    Login UI and view completed and register UI creating

--- users/views.py
+++ users/views.py
@@ -5,22 +5,24 @@ from django.contrib import messages
 
 
 def loginUser(request):
+    errors = {}
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-
-        check_user = User.objects.filter(username=username).exists()
+        
+        # returns boolean value if any value matching username exists in model User
+        check_user = User.objects.filter(username=username).exists() 
         if check_user:
-            authenticated_user = authenticate(
-                request, username=username, password=password
-            )
+            # for authenticating user with username and password
+            authenticated_user = authenticate(request, username=username, password=password)
             if authenticated_user:
-                login(username, password)
+                login(request, authenticated_user) # saves the user data in session 
                 messages.success(request, "You have successfully logged in")
-                return redirect("home")
+                return redirect("/home") # redirects to /home route
             else:
-                messages.error(request, "Invalid username or password")
-                return redirect('auth/log-in')
+                errors['password'] = "Invalid Password!" #stores error in key 'password'
         else:
-            messages.error(request, "User does not exist")
-            return redirect('auth/log-in')
+            errors['username'] = "User doesnot exist."
+        
+        if errors:
+            return render(request, 'pages/auth/login.html', {'errors': errors}) # renders login.html with errors
