ecbf843748808c8f6f9c635a190e87217f0bb9f5
Suman Regmi <limitlessplaying@gmail.com>
2025-06-29 12:06:22 +0545

    Created users' profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    class GenderOptions(models.TextChoices):
+        Male = "Male" , "Male"
+        Female = "Female", "Female"
+        Others = "Others", "Others"
+        
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(blank=True, null=True, max_length=50)
+    phone = models.CharField(blank=True, null=True, max_length=10)
+    nationality = models.CharField(blank=True, null=True, max_length=15, default="Nepal")
+    gender = models.CharField(choices=GenderOptions, default=GenderOptions.Male, max_length=6)
+    profile_image = models.ImageField( blank=True, null=True, upload_to="users")
+    dob = models.DateField(blank=True, null=True)
+        
