30eb67cc69222bcf4dc7cb9053be2714c92ef46e
Suman Regmi <limitlessplaying@gmail.com>
2025-07-23 11:50:54 +0545

    Unique views graph integration

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
@@ -25,10 +26,21 @@ def profilePage(request):
 
 @login_required(login_url='/auth/log-in/')
 def dashboard(request):
-    blogstats = BlogStats.objects.all()
-    blog_clicks = [b.blog_clicks for b in blogstats]
-    click_date = [b.created_at.strftime('%Y-%m-%d') for b in blogstats]
-    return render(request, 'pages/dashboard/writer/dashboard.html', {"blog_clicks":blog_clicks, "click_date":click_date})
+    user_blogs = Blog.objects.filter(author = request.user)
+    blogstats = BlogStats.objects.filter(blog__in = user_blogs).order_by('created_at')
+    click_data = defaultdict(int)
+    unique_data = defaultdict(int)
+    for b in blogstats:
+        date_str = b.created_at.strftime('%Y-%m-%d')
+        click_data[date_str] += b.blog_clicks
+        unique_data[date_str] += b.unique_views
+        
+    blog_clicks = list( click_data.values() )
+    click_date = list( click_data.keys() )
+    unique_views = list( unique_data.values() )
+    return render(request, 'pages/dashboard/writer/dashboard.html', {
+        "blog_clicks":blog_clicks, "click_date":click_date, "unique_views":unique_views
+        })
 
 def blogList(request):
     blogs = Blog.objects.filter( author = request.user).order_by('-created_at')
